a09451459bcb53da72b72f9ddc76fbc9697a40ce
AlexJohannson <mehedsanya@gmail.com>
2024-12-12 16:11:10 +0100

    [update]Lesson_3-type/Task_3-4-type/index.html, Main.ts, Main.js

---
+++ Lesson_3-type/Task_3-4-type/Main.js
@@ -0,0 +1,24 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var coursesAndDurationArray = [
+    { title: 'JavaScript Complex', monthDuration: 5 },
+    { title: 'Java Complex', monthDuration: 6 },
+    { title: 'Python Complex', monthDuration: 6 },
+    { title: 'QA Complex', monthDuration: 4 },
+    { title: 'FullStack', monthDuration: 7 },
+    { title: 'Frontend', monthDuration: 4 }
+];
+var sortCourses = coursesAndDurationArray
+    .sort(function (a, b) { return b.monthDuration - a.monthDuration; })
+    .filter(function (value) { return value.monthDuration > 5; })
+    .map(function (value, index) { return (__assign({ id: index + 1 }, value)); });
+console.log(sortCourses);
