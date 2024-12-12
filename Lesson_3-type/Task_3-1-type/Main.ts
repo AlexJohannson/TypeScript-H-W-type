178708aa1c83a6ebd1450c442b5a78c1a9ec3871
AlexJohannson <mehedsanya@gmail.com>
2024-12-12 15:59:09 +0100

    [update]Lesson_3-type/Task_3-1-type/index.html, Main.ts, Main.js

---
+++ Lesson_3-type/Task_3-1-type/Main.ts
@@ -0,0 +1,11 @@
+type listItem = string;
+type listOfItems = listItem[];
+
+
+let listOfItems: listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+document.write(`<ul>`);
+for (const item of listOfItems) {
+    document.write(`<li>${item}</li>`);
+}
+document.write(`</ul>`);
